6793af5b17349a4d4e1f63bec3fd0805d07648ac
bru_midori <brunamidoriyy@yahoo.com.br>
2022-12-28 15:53:11 -0300

    router basics

--- modI/conFusion/src/app/app.module.ts
+++ modI/conFusion/src/app/app.module.ts
@@ -7,16 +7,19 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
 
 import 'hammerjs';
 
 import { MenuComponent } from './menu/menu.component';
-import { AppComponent } from './app.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +39,9 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
